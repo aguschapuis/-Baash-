f667aa9329f961fdd32d9414880f16ffd8e1ceb1
TOMAS GABRIEL OSIECKI <tosiecki793@lab08.famaf.unc.edu.ar>
2019-09-03 18:09:14 -0300

    execute comentado para modificar

--- execute.c
+++ execute.c
@@ -1,14 +1,77 @@
 #include "execute.h"
+#include "builtin.h"
 #include <unistd.h>
 
 
 void execute_pipeline(pipeline apipe) {
    
+    if (builtin_index(apipe) != BUILTIN_UNKNOWN){
+      
+        builtin_run(apipe);
+   
+    } else {
+
+            scommand aux;
+            pipeline apipe2;
+            bstring execute;
+            int stdin;
+            int stdout;
+            int pid;
+            int pipes[2][n-1];
+
+            echo | sort  | uniq
+
+            if (pipeline_length(apipe) == 1){
+
+                 first = pipline_front(apipe);
+                 execute = (const bstring) first->list->data;
+                 scommand_pop_front(aux);
+                 pid = fork();
+                 if (pid == 0){
+                      // mirar los pipes a ver si tengo un lugar donde leer
+                      // Si hay un proceso antes
+                      //      -> leo del proceso anterior pipes[i-1][0]
+                      // Si hay un proceso despues
+                      //      -> escribo en pipes[i][1]
+
+                       execvp(execute, aux);
+                       exit(1);
+                 
+                 } else {
+                    
+                    if (pipeline_get_wait())
+                       wait();
+                 
+                 } 
+                 
+
+            } else {
+
+                 int a[2];
+                 pipe(a);
+                 pid = fork();
+                 if (pid == 0){
+                    // Soy el hijo
+                    // ejecutar el comando
+                 }
+            }
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     assert(apipe == NULL);
     int pid;
     scommand aux;
+    pipeline pipeaux;
     const char *command;
-    aux = scommand_front(apipe->list);
+    aux = scommand_front(apipe->list->data);
+    pipeaux = apipe;
     if (pipeline_length(apipe) == 1){
          pid = fork();
          if (pid == 0) {
@@ -18,9 +81,22 @@ void execute_pipeline(pipeline apipe) {
         } else {
             wait();
         }     
-    } else {
-        while (apipe->list != NULL){
-            
+    } else if (pipeline_length(apipe) == 2) {
+       
+             int a[2];
+             pipe(a);
+             pid = fork();
+             if(apipe->wait == true){
+                 if(pid == 0){
+                     command = aux->list->data;
+                     scommand_pop_front(aux);
+                     execvp(command, aux);
+                 } else {
+                     pipeline_pop_front(pipeaux);
+                     command = pipeaux->list->data;
+                 }
+
+
         }
     }
          

47cda41c935f0fee653c517c9bd3cc33d0315c50
AGUSTIN <aguschapuis@bitbucket.org>
2019-09-04 11:46:15 -0300

    builtin.c (TERMINADO)

--- builtin.c
+++ builtin.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 
 
+/*Esta primer funcion determina cual de los 3 valores de cmd_id le corresponde al pipe*/
+
 cmd_id builtin_index (const pipeline pipe){
     cmd_id index;
     if (bstricmp(scommand_front(pipeline_front(pipe)),bfromcstr("cd"))){
@@ -17,26 +19,34 @@ cmd_id builtin_index (const pipeline pipe){
 
 }
 
+/*Esta determina cuando tiene que salir (_exit)*/
+
 bool builtin_is_exit (const pipeline pipe){
     cmd_id index = builtin_index(pipe);
     return (index==BUILTIN_EXIT);
 
 }
 
+
+/*Corre el builtin , dependiendo cual sea su builtin_index */
+
 void builtin_run (const pipeline pipe){
     cmd_id index = builtin_index(pipe);
-    const_bstring dir;
+    const_bstring frst_scomand;
+    const char * dir;
     if(index==BUILTIN_CHDIR){
         pipeline_pop_front(pipe);
-        dir = scommand_front(pipeline_front(pipe));
+        frst_scomand = scommand_front(pipeline_front(pipe)); //primer scomand por eso el nombre 
+        dir = (const char *)frst_scomand->data; //direccion a la que tiene que dirijirse chdir
         chdir(dir);
+        pipeline_destroy(pipe);
     }
     else if (builtin_is_exit(pipe)){
         pipeline_destroy(pipe);
-        _exit(0);
+        _exit(0); //EXIT_SUCESS
     }
     else {
-        _exit(1);
+        _exit(1); //EXIT FAILURE
     }
     
 
